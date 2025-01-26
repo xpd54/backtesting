1872396609c838cd5ca19d158531f6a90044ea82
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-01-26 17:17:22 +0800

    Add method for order at specific price

--- src/base/account/account.cpp
+++ src/base/account/account.cpp
@@ -1,5 +1,7 @@
 #include "account.hpp"
 #include "common_interface/common.hpp"
+#include <algorithm>
+#include <cassert>
 namespace back_trader {
 
 float Floor(float amount, float unit) { return unit > 0 ? unit * std::floor(amount / unit) : amount; }
@@ -26,5 +28,147 @@ void Account::init_account(const AccountConfig &account_confg) {
     max_volume_ratio = account_confg.max_volume_ratio;
 }
 
-float Account::get_fee(const FeeConfig &fee_config, float quote_ammount) const {}
+float Account::get_fee(const FeeConfig &fee_config, float quote_amount) const {
+    return Ceil(std::max(fee_config.minimum_fee, fee_config.fixed_fee + quote_amount * fee_config.relative_fee),
+                quote_unit);
+}
+
+float Account::get_max_base_amount(const OhlcTick &ohlc_tick) const {
+    return max_volume_ratio > 0 ? Floor(max_volume_ratio * ohlc_tick.volume, base_unit)
+                                : std::numeric_limits<float>::max();
+}
+
+/*----------------------------------------- Price Of Type Of Orders ---------------------------------------------*/
+
+/* If liquidity is 1 buy order will execute at OHLC open price. If liquidity is 0 buy order will execute at OHLC high */
+float Account::get_market_buy_price(const OhlcTick &ohlc_tick) const {
+    return market_liquidity * ohlc_tick.open + (1.0f - market_liquidity) * ohlc_tick.high;
+}
+
+/*
+ *If liquidity is 1 sell order will execute at OHLC open price. If liquidity is 0 sell order will execute at OHLC low
+ */
+float Account::get_market_sell_price(const OhlcTick &ohlc_tick) const {
+    return market_liquidity * ohlc_tick.open + (1.0f - market_liquidity) * ohlc_tick.low;
+}
+
+/*
+ * If liquidity is 1 stop buy order will execute at OHLC open price . If liquidity is 0 stop buy order will execute at
+ * OHLC high
+ */
+float Account::get_stop_buy_price(const OhlcTick &ohlc_tick, float stop_price) const {
+    return market_liquidity * std::max(stop_price, ohlc_tick.open) + (1.0f - market_liquidity) * ohlc_tick.high;
+}
+
+/*
+ * If liquidity is 1 stop sell order will execute at OHLC open price. If liquidity is 0 stop sell order will execute at
+ * OHLC low
+ */
+float Account::get_stop_sell_price(const OhlcTick &ohlc_tick, float stop_price) const {
+    return market_liquidity * std::min(stop_price, ohlc_tick.open) + (1.0f - market_liquidity) * ohlc_tick.low;
+}
+
+/*------------------------------------- Order At Specific Price ---------------------------------------------*/
+
+bool Account::buy_base_currency(const FeeConfig &fee_config, float base_amount, float price) {
+    assert(price > 0);
+    assert(base_amount > 0);
+    base_amount = Round(base_amount, base_unit);
+
+    /*base unit is lowest denomination of base currency*/
+    if (base_amount < base_unit) {
+        return false;
+    }
+
+    const float quote_amount = Ceil(base_amount * price, quote_unit);
+    const float quote_fee = get_fee(fee_config, quote_amount);
+    const float total_quote_amount = quote_amount + quote_fee;
+
+    /*Account is holding less balance than buy price*/
+    if (total_quote_amount > quote_balance) {
+        return false;
+    }
+
+    // update account
+    base_balance = Round(base_balance + base_amount, base_unit);
+    quote_balance = Round(quote_balance - total_quote_amount, quote_unit);
+    total_fee = Round(total_fee + quote_fee, quote_unit);
+    return true;
+}
+
+bool Account::buy_at_quote(const FeeConfig &fee_config, float quote_amount, float price, float max_base_amount) {
+    assert(price > 0);
+    assert(quote_amount >= 0);
+    quote_amount = Round(quote_balance, quote_unit);
+
+    /* can't exicute when balance is low or amount is lower than lowest denomination*/
+    if (quote_amount < quote_unit || quote_amount > quote_balance) {
+        return false;
+    }
+    const float quote_fee = get_fee(fee_config, quote_amount);
+
+    /*all amount will get paid as fee*/
+    if (quote_amount < quote_fee) {
+        return false;
+    }
+
+    const float base_amount = Floor(std::min((quote_amount - quote_fee) / price, max_base_amount), base_unit);
+
+    /* buy base ammount is smaller than lowest denomination*/
+    if (base_amount < base_unit) {
+        return false;
+    }
+
+    return buy_base_currency(fee_config, base_amount, price);
+}
+
+bool Account::sell_base_currency(const FeeConfig &fee_config, float base_amount, float price) {
+    assert(price > 0);
+    assert(base_amount >= 0);
+    base_amount = Round(base_amount, base_unit);
+
+    /* can't sell lower than lowest denomination for base currency, or more than which account have in base balance*/
+    if (base_amount < base_unit || base_amount > base_balance) {
+        return false;
+    }
+    const float quote_amount = Floor(base_amount * price, base_unit);
+    const float quote_fee = get_fee(fee_config, quote_amount);
+    const float total_quote_amount = quote_amount - quote_fee;
+
+    /* After selling account is getting lower than lowest denomination.*/
+    if (total_quote_amount < quote_unit) {
+        return false;
+    }
+
+    // update account
+    base_balance = Round(base_balance - base_amount, base_unit);
+    quote_balance = Round(quote_balance + total_quote_amount, quote_unit);
+    total_fee = Round(total_fee + quote_fee, quote_unit);
+    return true;
+}
+
+bool Account::sell_at_quote(const FeeConfig &fee_config, float quote_amount, float price, float max_base_amount) {
+    assert(price > 0);
+    assert(quote_amount >= 0);
+    quote_amount = Round(quote_amount, quote_unit);
+    if (quote_amount < quote_unit) {
+        return false;
+    }
+
+    const float quote_fee = get_fee(fee_config, quote_amount);
+    const float base_amount = Floor(std::min((quote_amount + quote_fee) / price, max_base_amount), base_unit);
+
+    /* Selling lower ammount of base currency than lowest denomination of base currency*/
+    if (base_amount < base_unit) {
+        return false;
+    }
+
+    /*
+     * Note: When we sell base_amount of base currency, we receive at most:
+     * (quote_amount + quote_fee) - GetFee(quote_amount + quote_fee)
+     * Since GetFee(quote_amount) <= GetFee(quote_amount + quote_fee),
+     * We receive at most quote_amount of quote currency.
+     */
+    return sell_base_currency(fee_config, base_amount, price);
+}
 } // namespace back_trader
--- src/base/account/account.hpp
+++ src/base/account/account.hpp
@@ -10,7 +10,7 @@ struct Account {
     // balance in Quote currency (USD)
     float quote_balance = 0;
 
-    /*Total transaction fee over all executed order. Transaction fee is based on fee_config. It's totle quote ammount.
+    /*Total transaction fee over all executed order. Transaction fee is based on fee_config. It's totle quote amount.
      * It's subtracted from quote currency balance.*/
     float total_fee = 0;
 
@@ -29,17 +29,17 @@ struct Account {
     float market_liquidity = 1.0f;
 
     /* Fraction of OHLC tick volume that will be used to fill the limit order for the price. If volume (TOHLCV) *
-     * max_volume_ration < limit order volume than limit order would be filled partially in this given OHLC tick.*/
+     * max_volume_ratio < limit order volume than limit order would be filled partially in this given OHLC tick.*/
     float max_volume_ratio = 0.0f;
 
     /* Initialize the account with config*/
     void init_account(const AccountConfig &account_confg);
 
-    /* Fee in quote currency according to fee_config and given quote currency ammount in the transaciton.*/
-    float get_fee(const FeeConfig &fee_config, float quote_ammount) const;
+    /* Fee in quote currency according to fee_config and given quote currency amount in the transaciton.*/
+    float get_fee(const FeeConfig &fee_config, float quote_amount) const;
 
-    /* Maximum tradeable base currency (BTC) ammount (volume) based on max_volume_ratio on given ohlc_tick*/
-    float get_max_base_ammount(const OhlcTick &ohlc_tick) const;
+    /* Maximum tradeable base currency (BTC) amount (volume) based on max_volume_ratio on given ohlc_tick*/
+    float get_max_base_amount(const OhlcTick &ohlc_tick) const;
 
     /*----------------------------------------- Price Of Type Of Orders ---------------------------------------------*/
 
@@ -57,73 +57,73 @@ struct Account {
 
     /*------------------------------------- Order At Specific Price ---------------------------------------------*/
 
-    /* Buy ammount of base currency at given price*/
-    bool buy_base_currency(const FeeConfig &fee_config, float base_ammount, float price);
+    /* Buy amount of base currency at given price*/
+    bool buy_base_currency(const FeeConfig &fee_config, float base_amount, float price);
 
-    /* Buy as much of base currency(BTC) as possible at given price. Using most quote_ammount (USD), It's possible to
-     * buy most max_base_ammount base.*/
-    bool buy_at_quote(const FeeConfig &fee_config, float quote_ammount, float price,
-                      float max_base_ammount = std::numeric_limits<float>::max());
+    /* Buy as much of base currency(BTC) as possible at given price. Using most quote_amount (USD), It's possible to
+     * buy most max_base_amount base.*/
+    bool buy_at_quote(const FeeConfig &fee_config, float quote_amount, float price,
+                      float max_base_amount = std::numeric_limits<float>::max());
 
-    /* Sell ammount of base currency at given price.*/
-    bool sell_base_currency(const FeeConfig &fee_config, float base_ammount, float price);
+    /* Sell amount of base currency at given price.*/
+    bool sell_base_currency(const FeeConfig &fee_config, float base_amount, float price);
 
     /* Sell as much of base currency as possible at given price. Getting at most quote_ammout. It's possible to sell
-     * max_base_ammount at the price*/
-    bool sell_at_quote(const FeeConfig &fee_config, float quote_ammount, float price,
-                       float max_base_ammount = std::numeric_limits<float>::max());
+     * max_base_amount at the price*/
+    bool sell_at_quote(const FeeConfig &fee_config, float quote_amount, float price,
+                       float max_base_amount = std::numeric_limits<float>::max());
 
     /*------------- Execute Market Orders (Buy/Sell at market price itself) -----------------------------------*/
 
-    /* Execute market buy order of base_ammount (volume of btc). (I want to buy 0.04 BTC)*/
+    /* Execute market buy order of base_amount (volume of btc). (I want to buy 0.04 BTC)*/
     bool market_buy(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_ammout);
 
-    /* Execute market buy order spending most of quote_ammount (USD). (I want to buy $100 worth of BTC/ may be just for
+    /* Execute market buy order spending most of quote_amount (USD). (I want to buy $100 worth of BTC/ may be just for
      * $94 depending on base_unit)*/
-    bool market_buy_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_ammount);
+    bool market_buy_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_amount);
 
-    /* Execute market sell order of base_ammount (BTC). (I want to sell 0.04 BTC)*/
-    bool market_sell(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_ammount);
+    /* Execute market sell order of base_amount (BTC). (I want to sell 0.04 BTC)*/
+    bool market_sell(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_amount);
 
-    /* Execute market sell order receiving at most quote_ammount. (I want to sell $100 worth of BTC)*/
-    bool market_sell_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_ammount);
+    /* Execute market sell order receiving at most quote_amount. (I want to sell $100 worth of BTC)*/
+    bool market_sell_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_amount);
 
     /*------------- Execute Stop Orders (Buy/Sell When Market Rise/Fall To Stop Price) ---------------------------*/
     // (https://www.investopedia.com/terms/s/stoporder.asp)
-    /* Execute stop buy order of base ammount at stop_price. Stop Buy only get executed when price Rise to stop_price
+    /* Execute stop buy order of base amount at stop_price. Stop Buy only get executed when price Rise to stop_price
      * price. (When given OHLC high price is >= stop_price) */
     bool stop_buy(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_amount, float stop_price);
 
-    /* Execute stop buy order of quote_ammount of quote currency at stop_price. Stop Buy only get executed when price
+    /* Execute stop buy order of quote_amount of quote currency at stop_price. Stop Buy only get executed when price
      * RISE to stop_price price. (When given OHLC high price is >= stop_price) */
     bool stop_buy_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_amount,
                            float stop_price);
 
-    /* Execute stop sell order of base_ammount in base currency (BTC) at stop_price. Stop Sell only get executed when
+    /* Execute stop sell order of base_amount in base currency (BTC) at stop_price. Stop Sell only get executed when
      * price FALLS to stop_price price. (When given OHLC low price is <= stop_price) */
     bool stop_sell(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_amount, float stop_price);
 
-    /* Execute stop sell order of quote_ammount in quote currency (USD) at stop_price. Stop Sell only get executed when
+    /* Execute stop sell order of quote_amount in quote currency (USD) at stop_price. Stop Sell only get executed when
      * price FALLS to stop_stop price. (When given OHLC low price is <= stop_price) */
     bool stop_sell_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_amount,
                             float stop_price);
 
     /*------------- Execute Limit Orders (Buy/Sell When Market Rise/Fall To Stop Price) ---------------------------*/
 
-    /* Execute limit buy order of base ammount at limit_price. Limit Buy only get executed when price FALLS to
+    /* Execute limit buy order of base amount at limit_price. Limit Buy only get executed when price FALLS to
      * limit_price price. (When given OHLC low price is <= limit_price) */
     bool limit_buy(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_amount, float limit_price);
 
-    /* Execute limit buy order of quote_ammount of quote currency at limit_price. Limit Buy only get executed when price
+    /* Execute limit buy order of quote_amount of quote currency at limit_price. Limit Buy only get executed when price
      * FALLS to limit_price price. (When given OHLC low price is <= limit_price) */
     bool limit_buy_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_amount,
                             float limit_price);
 
-    /* Execute limit sell order of base_ammount in base currency (BTC) at limit_price. Limit Sell only get executed when
+    /* Execute limit sell order of base_amount in base currency (BTC) at limit_price. Limit Sell only get executed when
      * price RISE to limit_price price. (When given OHLC high price is >= limit_price) */
     bool limit_sell(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float base_amount, float limit_price);
 
-    /* Execute limit sell order of quote_ammount in quote currency (USD) at limit_price. Limit Sell only get executed
+    /* Execute limit sell order of quote_amount in quote currency (USD) at limit_price. Limit Sell only get executed
      * when price RISE to limit_price price. (When given OHLC high price is >= limit_price) */
     bool limit_sell_at_quote(const FeeConfig &fee_config, const OhlcTick &ohlc_tick, float quote_amount,
                              float limit_price);
